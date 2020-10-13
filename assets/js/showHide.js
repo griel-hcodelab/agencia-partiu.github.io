3d3f193cc5f868a2f98ed5d7aca232aab4929d23
Roberto Griel Filho <roberto@griel.com.br>
2020-10-13 16:47:10 -0300

    Dropdown da conta, ajustes no Index, ajustes no JS

--- assets/js/showHide.js
+++ assets/js/showHide.js
@@ -1,12 +1,14 @@
 function show(element = Array()) {
     element.forEach(item => {
         console.log(item);
-        document.querySelector(item).classList.toggle('open');
+        document.querySelector(item).classList.add('open');
+        document.querySelector(item).classList.remove('close');
     });
 }
 function hide(element = Array()) {
     element.forEach(item => {
         console.log(item);
-        document.querySelector(item).classList.toggle('close');
+        document.querySelector(item).classList.add('close');
+        document.querySelector(item).classList.remove('open');
     });
 }
