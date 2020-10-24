060a56122e8f0774620961639713052406ca52ef
robzgf <rgrielfilho@gmail.com>
2020-10-23 21:22:43 -0300

    Fechar o menu quando clicar fora

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,3 +7,21 @@ document.querySelector("#menu-button").addEventListener('click', e => {
         document.querySelector("body").style.overflow = 'hidden';
     }
 });
+document.querySelector(".overlay").addEventListener('click', function(){
+    document.querySelector(".menu").classList.toggle('open');
+    document.querySelector(".overlay").classList.toggle('open');
+    if (document.querySelector("body").style[0] == "overflow-x") {
+        document.querySelector("body").style = '';
+    } else {
+        document.querySelector("body").style.overflow = 'hidden';
+    }
+});
+document.querySelector(".menu").addEventListener('click', function(){
+    document.querySelector(".menu").classList.toggle('open');
+    document.querySelector(".overlay").classList.toggle('open');
+    if (document.querySelector("body").style[0] == "overflow-x") {
+        document.querySelector("body").style = '';
+    } else {
+        document.querySelector("body").style.overflow = 'hidden';
+    }
+});
